9639cd047454fb4152e1b247e52f27f460b40a00
Romina <romina.pati@gmail.com>
2022-06-21 17:57:06 -0400

    Add Speakers structure

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const speakersInfo = `
+<li class="speaker-info"> 
+                    <ul>
+                        <li><img src="/images-capstone/speaker8.jpg" alt="speaker 8" /></li>
+                        <li>
+                            <div class="speaker-name">Yochai Benkler</div>
+                            <div class="speaker-cv">Lorem ipsum dolor sit amet consectetur.</div>
+                            <div class="speaker-data">
+                                <p>Lorem ipsum, dolor sit amet consectetur.
+                                Eos aspernatur, nesciunt dolores, officia, eum quae at illum error
+                                labore earum nihil harum enim unde deleniti delectus fuga iste quibusdam voluptatum.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </li>`;
